6dfae59a2b2d723e1607769c48e5d84f5f3b6610
MatthewFloyd <floydmatt22@gmail.com>
2019-07-27 16:56:17 -0600

    Block partial breaks + unbreakables

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +7,46 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparkleVFX;
+    [SerializeField] Sprite[] hitSprites;
 
     Level level;
     GameSession gameStatus;
+
+    private int timesHit = 0;
 
     private void Start()
     {
         gameStatus = FindObjectOfType<GameSession>();
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            level.CountBreakableBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (tag == "Breakable")
+        {
+            timesHit++;
+            int maxHits = hitSprites.Length + 1;
+            if (timesHit >= maxHits)
+            {
+                DestroyBlock();
+            }
+            else
+            {
+                ShowNextHitSprite();
+            }
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if(hitSprites[spriteIndex] == null) { Debug.LogError("Block Sprite is missing from array " + gameObject.name); }
+        else
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
